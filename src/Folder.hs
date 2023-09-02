1b5e34721fdf3bb45555f088f04d41a28fca6860
data-miner00 <data-miner00@users.noreply.github.com>
2023-09-02 09:10:32 +0800

    suffer brain damange

--- src/Folder.hs
+++ src/Folder.hs
@@ -7,6 +7,8 @@ module Folder
     , length'
     , length''
     , map'
+    , rev
+    , prefixes
     ) where
 
 
@@ -35,8 +37,60 @@ length'' lst = foldr (const $ (+) 1) 0 lst
 map' :: (a -> b) -> [a] -> [b]
 map' f lst = foldr ((:) . f) [] lst
 
+
+count' :: (Foldable t, Eq a, Num b) => a -> t a -> b
+count' e = foldr (\x acc -> if x == e then acc + 1 else acc) 0
+
 {-  Folding on trees
     1. In-order
     2. Post-order
     3. Pre-order
 -}
+
+-- Fold exercise: https://www.youtube.com/watch?v=46dksIrx6jQ
+rev :: [a] -> [a]
+rev = foldl (\acc x -> x : acc) []
+
+prefixes :: [a] -> [[a]]
+prefixes = foldr (\x acc -> [x] : map (x :) acc) []
+
+prefixes' :: [a] -> [[a]]
+prefixes' = tail . scanl (\acc x -> acc ++ [x]) []
+
+prefixes'' :: [a] -> [[a]]
+prefixes'' = (\(_:xs) -> xs) . foldl (\acc x -> acc ++ [last acc ++ [x]]) [[]]
+
+lagrange :: [(Float, Float)] -> Float -> Float
+lagrange xs x = foldl (\acc (xj, y) -> acc + (y * l xj)) 0 xs
+  where
+    l xj = foldl (
+        \acc (xk, _) ->
+            if xj == xk then
+                acc
+            else
+                acc * ((x - xk) / (xj - xk))) 1 xs
+
+lagrange' :: (Foldable t, Eq b, Fractional b) => t (b, b) -> b -> b
+lagrange' k xp = foldr (\(x,y) acc -> acc + y * foldr (\(x1,_) acc1 -> if x /= x1 then acc1*(xp-x1) / (x-x1) else acc1) 1 k) 0 k
+
+-- Prefix tree
+data Trie a = Leaf a | Node a [Trie a]
+
+t :: Trie Char
+t =
+    Node 'c' [
+        Node 'a' [
+            Leaf 'r', Leaf 't'
+        ],
+        Node 'o' [
+            Node 'o' [
+                Leaf 'l'
+            ]
+        ]
+    ]
+
+foldtrie :: (b -> a -> b) -> b -> Trie a -> b
+foldtrie f acc (Leaf x) = f acc x
+foldtrie f acc (Node x xs) = foldl f' (f acc x) xs
+  where
+    f' acc t = foldtrie f acc t
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Folder
 
 
 main :: IO ()
@@ -16,3 +17,11 @@ main = hspec $ do
         
         it "throws an exception if used with an empty list" $ do
             evaluate (head []) `shouldThrow` anyException
+
+    describe "Folder" $ do
+        it "should return reversed list" $ do
+            rev [1, 2, 3] `shouldBe` ([3, 2, 1] :: [Int])
+
+        it "should return properly prefixed list" $ do
+            prefixes [1, 2, 3] `shouldBe` ([[1], [1, 2], [1, 2, 3]] :: [[Int]])
+        
